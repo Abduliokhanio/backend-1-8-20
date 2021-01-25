2e2a383d59704148076d61e1777638d4d1a3a018
AbdulioKhanio <Akhan17@outlook.com>
2021-01-25 15:02:18 -0600

    added notes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,19 +2,19 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :update, :destroy]
 
   # GET /blogs
-  def index
+  def index #returns all blogs 
     @blogs = Blog.all.reverse
 
     render json: @blogs
   end
 
   # GET /blogs/1
-  def show
+  def show #returs a particualr blog
     render json: @blog
   end
 
   # POST /blogs
-  def create
+  def create #creates a blog with the prams 
     @blog = Blog.new(blog_params)
 
     if @blog.save
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
   end
 
   # PATCH/PUT /blogs/1
-  def update
+  def update # updates a blog with the params 
     if @blog.update(blog_params)
       render json: @blog
     else
@@ -34,18 +34,18 @@ class BlogsController < ApplicationController
   end
 
   # DELETE /blogs/1
-  def destroy
+  def destroy # deletes a particular blog
     @blog.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_blog
+    def set_blog # find the information on the particualr blog we want to show/update/delete with the previously mentioned before action
       @blog = Blog.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
-    def blog_params
+    def blog_params # hold the parameters the blog can take in to build a new blog 
       params.require(:blog).permit(:title, :description)
     end
 end
